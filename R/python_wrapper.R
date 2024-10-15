bed20e084c6f51a1b7df67e51f27730cdb25202d
htulet <hadrien.tulet@cirad.fr>
2024-10-15 16:05:10 +0200

    auto env install on import

--- R/python_wrapper.R
+++ R/python_wrapper.R
@@ -1,18 +1,24 @@
-library(reticulate)
-path_to_env <- "D:/tulet/envs/pipeline"   #Put the name of your python env here
-use_python(path_to_env)
-
-
-source_python(system.file("__init__.py", package = "managecrownsdata"))
+#' Check the crown file compatibility
+#'
+#' @param crownFile A sf object
+#'
+#' @return Text that give you information about your file.
+#' Indicates whether your file will be compatible or not  for the other functions of the package.
+#' Pay attention to the line starting with '-- ERROR --'.
+#' @export
+#' @import reticulate
 
 
 #à faire pareil pour les autres fonctions
-arosics_in_r <- function(path_in, ref_filepath, out_dir_path, 
-    corr_type = "global", max_shift = 250L, max_iter = 100L, 
+arosics_in_r <- function(path_in, ref_filepath, out_dir_path,
+    corr_type = "global", max_shift = 250L, max_iter = 100L,
     grid_res = 1000L, window_size = NULL, window_pos = list(NULL, NULL),
-    mp = NULL, save_data = TRUE, save_vector_plot = FALSE, 
+    mp = NULL, save_data = TRUE, save_vector_plot = FALSE,
     dynamic_corr = FALSE, apply_matrix = FALSE) {
-    
+
+
+    source_python(system.file("__init__.py", package = "managecrownsdata"))
+
     complete_arosics_process(path_in = path_in,
                             ref_filepath = ref_filepath,
                             out_dir_path = out_dir_path,
---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+.onLoad <- function(libname, pkgname) {
+   library(reticulate)
+
+   env_name <- getOption("managecrownsdata.conda_env", "pipeline_test_R")
+
+   if (!(env_name %in% conda_list()$name)) {
+      message("Creating conda environment '", env_name, "'...")
+      conda_create(env_name, environment = "../environment.yaml")
+   }
+
+   use_condaenv(env_name, required = TRUE)
+}
