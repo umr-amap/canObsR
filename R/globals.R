84ab0f7370cd4a1e189a04177b91d4b1ac22c34b
Hugolebl <hugo.leblanc53@gmail.com>
2024-10-22 16:30:00 +0100

    new commit

--- R/globals.R
+++ R/globals.R
@@ -1,7 +1,7 @@
 utils::globalVariables(c(".", "type", "metric","band","value",'family','genus',
                          "specie","plot_name","code_sp","st_contains","crowns",
                          "text","obs","update",'sp','site','Comm','Usable_crown',
-                         'phenophase','repro','complete_arosics_process'))
+                         'phenophase','repro','complete_arosics_process','na'))
 
 
 
