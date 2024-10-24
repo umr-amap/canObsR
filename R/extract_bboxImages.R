32fe5e76c90959cde4edc6a77f9ca4b0c7fac4a1
Hugolebl <hugo.leblanc53@gmail.com>
2024-10-24 17:24:21 +0100

    new commit

--- R/extract_bboxImages.R
+++ R/extract_bboxImages.R
@@ -4,29 +4,43 @@
 #' an \code{sf} object with one polygon per image.
 #'
 #'
-#' @param RGB_paths list. a list with the full paths to the RGB rasters.
+#' @param path_in vector with the full paths to the RGB rasters images.
 #' @param crs crs. Object of class 'crs', could be get from st_crs(..). If NULL,
 #'  it will use and transform all the data into the crs of the first RGB image.
 #' @param date chr. Vector with dates (format should be '%Y_%m_%d', p.e
 #'  '2022_09_25'). The order of the dates should match with the order of the
-#'  dates of the image in the RGB_paths.
+#'  dates of the image in the path_in
 #' @param directory chr. The path to the directory used to export the vectors.
 #' One file will be export per date. If NULL, the vector will not be exported.
 #' @param filename logical. The name of the files.
 #' @export
 #' @importFrom terra rast
 #' @importFrom terra as.polygons
+#' @importFrom magrittr "%>%"
 #' @import sf
 #' @import dplyr
 
 
 extract_bboxImages <-
 
-   function(RGB_paths, crs, date, directory = NULL, filename = NULL){
+   function(path_in, crs = NULL, date = NULL, directory = NULL, filename = NULL){
 
-      for (i in 1:length(RGB_paths)) {
 
-         r = terra::rast(RGB_paths[i], lyrs = 1)
+   # Check if the user has added a crs, if not it automatically find it --------
+
+      if ( is.null(crs) ) {crs = sf::st_crs (sf::st_read (path_in[1])) }
+
+
+   # Check if the user has added dates, if not it will be 1 to x -------------
+
+      if ( is.null(date) ) {date = paste0('date_',1:length(path_in)) }
+
+
+   # For each image, extract the non NA values bounding box ------------------
+
+      for (i in 1:length(path_in)) {
+
+         r = terra::rast(path_in[i], lyrs = 1)
          r[[1]][!is.na(r[[1]])] = 1
 
          poly <- terra::as.polygons(r[[1]])
@@ -42,6 +56,7 @@ extract_bboxImages <-
             dplyr::mutate(date = date[i])
 
          if(is.null(filename)){filename = 'NonNA_area'}
+
          if(!is.null(directory)){
 
             sf::write_sf(bbox, paste0(directory, filename ,'_', date[i], '.gpkg') )
--- R/extract_crownsImages.R
+++ R/extract_crownsImages.R
@@ -1,18 +1,21 @@
 #'Extract crowns images
 #'
 #'@description The function extracts and save .jpeg images for each crown at
-#'  each date.
+#'each date.
 #'
-#'@param crownFile A \code{sf} object for the crowns with an 'id' variable.
-#'@param RGB_paths a list with the full paths to the RGB rasters.
-#'@param site chr. name of the site, p.e 'Mbalmayo'.
-#'@param directory chr. The path to the directory use to stored the images. The
+#'@param path_images A \code{sf} object for the crowns with an 'id' variable.
+#'@param path_crownFile a list with the full paths to the RGB rasters.
+#'@param path_bbox xx
+#'@param path_out chr. The path to the directory use to stored the images. The
 #'  function will create the folder, It doesn't need to exists.
+#'@param site chr. name of the site, p.e 'Mbalmayo'.
 #'@param date chr. Vector with dates (format should be '%Y_%m_%d', p.e
 #'  '2022_09_25'). The order of the dates should match with the order of the
-#'  dates of the image in the RGB_paths.
+#'  dates of the image in the path_in.
 #'@param crs crs. Object of class 'crs', could be get from st_crs(..). If NULL,
 #'  it will use and transform all the data into the crs of the first RGB image.
+#'@param parallel xx
+#'@param update xx
 #'@param specific_quality Logical, if TRUE the quality of image will be
 #'  determine by the height and width arguments.
 #'@param height if specific_quality = TRUE, the height of the device
@@ -36,19 +39,19 @@
 #' crownFile <- st_read("/processed_shp/Mbalmayo_crowns_2023_11_08.gpkg")
 #' site = 'Mbalmayo'
 #' crs = 'EPSG:32632'
-#' RGB_paths = list.files('F:/VIA/Cameroun/Mbalmayo/Pheno/RGB', pattern = "\\.tif$", full.names = TRUE)
+#' path_in = list.files('F:/VIA/Cameroun/Mbalmayo/Pheno/RGB', pattern = "\\.tif$", full.names = TRUE)
 #' date <- as.Date (
 #' sapply(
-#' str_split(basename(RGB_paths),'_'), function(x) paste0(x[2],x[3],x[4])
+#' str_split(basename(path_in),'_'), function(x) paste0(x[2],x[3],x[4])
 #' ), "%Y%m%d"
 #' )
-#' crownFile <- st_transform(crownFile, st_crs( read_stars(RGB_paths[1],proxy = T)))
+#' crownFile <- st_transform(crownFile, st_crs( read_stars(path_in[1],proxy = T)))
 #'
 #' # Default parameters (quality of image : 720*825 pixels)
 #'
 #' extract_crownsImages(
 #'    crownFile = crownFile,
-#'    RGB_paths = RGB_paths,
+#'    path_in = path_in,
 #'    directory = directory,
 #'    date = date,
 #'    tx_sp_lvl = 'tx_sp_lvl',
@@ -59,7 +62,7 @@
 #'
 #' extract_crownsImages(
 #'    crownFile = crownFile,
-#'    RGB_paths = RGB_paths,
+#'    path_in = path_in,
 #'    directory = directory,
 #'    date = date,
 #'    tx_sp_lvl = 'tx_sp_lvl',
@@ -79,58 +82,47 @@
 #'@importFrom terra plotRGB
 #'@importFrom grDevices jpeg
 #'@importFrom grDevices dev.off
+#' @importFrom magrittr "%>%"
 #'@import sf
 #'@import dplyr
+
 
 
 
 extract_crownsImages <-
 
    function(
-      crownFile,
-      RGB_paths,
+      path_images,
+      path_crownFile,
+      path_bbox,
+      path_out,
       site = NULL,
       date = NULL,
       crs = NULL,
-      directory = NULL,
+      parallel = FALSE,
+      update = FALSE,
       specific_quality = FALSE,
       width = 250,
       height = 300
    ){
 
-      within_crowns <- list()
       crownFile <- crownFile %>% sf::st_transform(crs = crs)
-
-      for (i in 1:length(RGB_paths)) {
-
-         bbox <- create_bbox_rast (raster_path = RGB_paths[i], crs = crs)
-
-         within_crowns[[paste0(date[i])]] <- sf::st_join(bbox, crownFile, join = st_contains) %>% .[['id']]
-
-      }
-
-      # if(site == 'Bouamir'){
-      #
-      #    img6 <- mosaic_bouamir_adjust(RGB_path_tocorrect = RGB_paths[6], RGB_path_model = RGB_paths[1])
-      #    img12 <- mosaic_bouamir_adjust(RGB_path_tocorrect = RGB_paths[12], RGB_path_model = RGB_paths[1])
-      #
-      # }
+      bbox <- lapply(Bbox_path, st_read)
 
 
       for (i in 1:length(unique(crownFile$id))) {
 
          tmp_id <- crownFile$id[i]
-         tmp_sp <- crownFile %>% dplyr::filter(id == tmp_id) %>% .[["tx_sp_lvl"]]
-         tmp_crown <- crownFile %>% dplyr::filter(id == tmp_id)
+         tmp_sp <- crownFile$tx_sp_lvl[i]
+         if(is.na(tmp_sp)){ tmp_sp <- paste(crownFile$tax_gen[i],'sp') }
+         tmp_crown <- crownFile[i,]
          tmp_dir <- paste0(directory, "/crown_", tmp_id, "_", tmp_sp)
 
          dir.create(tmp_dir)
 
-         bbox <- create_bbox_shp (shp = tmp_crown)
+         crown_bbox <- create_bbox_shp (shp = tmp_crown)
 
-         for (j in 1:length(RGB_paths)) {
-
-            x <- stars::read_stars(RGB_paths[j], proxy = T)[bbox][, , , 1:3]
+         for (j in 1:length(path_in)) {
 
             if (specific_quality == FALSE) {
                grDevices::jpeg(file = file.path(
@@ -148,16 +140,17 @@ extract_crownsImages <-
                height = height)
             }
 
-            if (!(tmp_id %in% within_crowns[[j]]) |
-               is.nan(mean(as.data.frame(x)[, 4], na.rm = T))) {
-               plot_nodata()
+            if (st_intersects(bbox[[j]], crown_bbox, sparse = F)) {
 
-            } else {
+
+               x <- stars::read_stars(path_in[j], proxy = T)[crown_bbox][, , , 1:3]
+
                terra::plotRGB(
                   terra::rast(x),
                   main = paste(date[j], "     ", tmp_sp, "     id =", tmp_id),
-                  ext = bbox,
-                  axes = T
+                  ext = sf::st_as_sf(crown_bbox),
+                  axes = T,
+                  mar = 2
                )
                base::plot(
                   tmp_crown$geom,
@@ -166,7 +159,12 @@ extract_crownsImages <-
                   add = T
                )
 
-            }
+
+            } else {
+
+               plot_nodata()
+
+               }
 
             grDevices::dev.off()
          }
