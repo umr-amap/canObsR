4b68ce37cd260f26e701634cc7cc5d252fb5457d
Hugolebl <hugo.leblanc53@gmail.com>
2024-11-13 16:38:56 +0100

    new commit

--- R/plot_signal.R
+++ R/plot_signal.R
@@ -3,15 +3,16 @@
 #' @description A fct function
 #'
 #' @param data A \code{tibble} or \code{dataframe} of long data format for labels
-#' @param Species chr
-#' @param Genus chr
-#' @param Family chr
-#' @param Type chr
-#' @param Metric chr
-#' @param Band chr
-#' @param facet_by chr
-#' @param slcted_id chr
-#' @param title chr
+#' @param Species chr Filtering the data at the species level
+#' @param Genus chr Filtering the data at the genus level
+#' @param Family chr Filtering the data at the family level
+#' @param Type chr Filtering by the type
+#' @param Metric chr Filtering by the metric. By defaut it is the mean.
+#' @param Band chr Filtering by the band.
+#' @param facet_by chr Facetting by a variable
+#' @param slcted_id chr Highlighting an id
+#' @param show_Labels description
+#' @param title chr The title of the plot
 #'
 #' @return return a ggplot
 #'
@@ -27,7 +28,8 @@ plot_signal <- function(data,
                        Metric = 'mean',
                        Band = NULL,
                        facet_by = 'band',
-                       slcted_id = 'all',
+                       slcted_id = NULL,
+                       show_Labels = FALSE,
                        title = NULL
 ) {
 
@@ -41,7 +43,7 @@ plot_signal <- function(data,
       { if (!is.null(Band)) dplyr::filter (., band %in% Band) else . }
 
 
-   if(slcted_id != 'all') {
+   if(!is.null(slcted_id)) {
 
       data <- data %>% dplyr::mutate( highlight = ifelse(id==slcted_id, paste(slcted_id), "Other"))
 
@@ -54,39 +56,39 @@ plot_signal <- function(data,
 
    ggplot2::ggplot (data) +
 
-      {if ( slcted_id == 'all' & !is.null(facet_by))   ggplot2::geom_line (data = data, ggplot2::aes (x = date, y = value, group = id, colour = band) ) } +
+      {if ( is.null(slcted_id) & !is.null(facet_by))   ggplot2::geom_line (data = data, ggplot2::aes (x = date, y = value, group = id, colour = band) ) } +
 
-      {if ( slcted_id == 'all' &  is.null(facet_by) )   ggplot2::geom_line (data = data, ggplot2::aes (x = date, y = value, group = id) ) } +
+      {if ( is.null(slcted_id) &  is.null(facet_by) )   ggplot2::geom_line (data = data, ggplot2::aes (x = date, y = value, group = id) ) } +
 
 
-      {if ( slcted_id != 'all' &  is.null(facet_by) )
+      {if ( !is.null(slcted_id) &  is.null(facet_by) )
          ggplot2::geom_line (data = dplyr::filter(data, highlight != paste(slcted_id) ),
                              ggplot2::aes(x = date, y = value, group = id),
                              colour = "lightgrey",
                              linewidth = 1)
       } +
 
-      {if ( slcted_id != 'all' &  !is.null(facet_by) )
+      {if ( !is.null(slcted_id) &  !is.null(facet_by) )
          ggplot2::geom_line (data = dplyr::filter(data, highlight != paste(slcted_id) ),
                              ggplot2::aes(x = date, y = value, group = id),
                              colour = "lightgrey",
                              linewidth = 1)
       } +
 
-      {if ( slcted_id != 'all' &  is.null(facet_by) )
+      {if ( !is.null(slcted_id) &  is.null(facet_by) )
          ggplot2::geom_line(data = dplyr::filter(data, highlight == paste(slcted_id)),
                             ggplot2::aes(x = date, y = value, group = id),
                             size = 2.5)
       } +
 
-      {if ( slcted_id != 'all' &  !is.null(facet_by) )
+      {if (!is.null(slcted_id) &  !is.null(facet_by) )
          ggplot2::geom_line(data = dplyr::filter(data, highlight == paste(slcted_id)),
                             ggplot2::aes(x = date, y = value, group = id, colour = band),
                             size = 2.5)
       } +
 
 
-      {if (slcted_id != 'all' )
+      {if (!is.null(slcted_id) & show_Labels)
          ggplot2::geom_point (data = dplyr::filter(data, highlight == paste(slcted_id) ),
                               ggplot2::aes(x = date, y = y_lvl, colour = phenophase),
                               na.rm=TRUE, fontface = "bold", size = 2)
@@ -94,7 +96,7 @@ plot_signal <- function(data,
 
       {if ( !is.null(facet_by) )  ggplot2::facet_grid( rows =  vars(get(facet_by)), scales = 'free') } +
 
-      {if ( slcted_id != 'all' )
+      {if ( !is.null(slcted_id) )
          ggplot2::scale_fill_discrete()
       }  +
 
