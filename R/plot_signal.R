5b51a16da31b25fec351b61fa1b77f8833af9c38
Hugolebl <hugo.leblanc53@gmail.com>
2025-06-03 09:54:31 +0100

    rename data argument

--- R/plot_signal.R
+++ R/plot_signal.R
@@ -2,7 +2,7 @@
 #'
 #' @description A fct function
 #'
-#' @param Labels tbl_df. Labels Labels
+#' @param data tbl_df. Labels Labels
 #' @param Species character. Specifying the species you want to filter
 #' @param Genus character. Specifying the genus you want to filter
 #' @param Family character. Specifying the family(ies) you want to filter
@@ -22,7 +22,7 @@
 #' @import ggplot2
 #' @importFrom stats quantile
 
-plot_signal <- function(Labels,
+plot_signal <- function(data,
                        Species = NULL,
                        Genus = NULL,
                        Family = NULL,
@@ -36,7 +36,7 @@ plot_signal <- function(Labels,
 ) {
 
 
-   Labels <- Labels %>%
+   data <- data %>%
       { if (!is.null(Species)) dplyr::filter (., species %in% Species) else . } %>%
       { if (!is.null(Genus)) dplyr::filter (., genus %in% Genus) else . } %>%
       { if (!is.null(Family)) dplyr::filter (., family %in% Family) else . } %>%
@@ -47,51 +47,51 @@ plot_signal <- function(Labels,
 
    if(!is.null(slcted_id)) {
 
-      Labels <- Labels %>% mutate( highlight = ifelse(id==slcted_id, paste(slcted_id), "Other"))
+      data <- data %>% mutate( highlight = ifelse(id==slcted_id, paste(slcted_id), "Other"))
 
-      Labels <- Labels %>%
+      data <- data %>%
          group_by(id, band) %>%
          mutate(y_lvl = (quantile(value, na.rm = T, probs = 0.1) )) %>%
          ungroup()
    }
 
 
-   ggplot (Labels) +
+   ggplot (data) +
 
-      {if ( is.null(slcted_id) & !is.null(facet_by))   geom_line (data = Labels, aes (x = date, y = value, group = id, colour = band) ) } +
+      {if ( is.null(slcted_id) & !is.null(facet_by))   geom_line (data = data, aes (x = date, y = value, group = id, colour = band) ) } +
 
-      {if ( is.null(slcted_id) &  is.null(facet_by) )   geom_line (data = Labels, aes (x = date, y = value, group = id) ) } +
+      {if ( is.null(slcted_id) &  is.null(facet_by) )   geom_line (data = data, aes (x = date, y = value, group = id) ) } +
 
 
       {if ( !is.null(slcted_id) &  is.null(facet_by) )
-         geom_line (data = dplyr::filter(Labels, highlight != paste(slcted_id) ),
+         geom_line (data = dplyr::filter(data, highlight != paste(slcted_id) ),
                              aes(x = date, y = value, group = id),
                              colour = "lightgrey",
                              linewidth = 1)
       } +
 
       {if ( !is.null(slcted_id) &  !is.null(facet_by) )
-         geom_line (data = dplyr::filter(Labels, highlight != paste(slcted_id) ),
+         geom_line (data = dplyr::filter(data, highlight != paste(slcted_id) ),
                              aes(x = date, y = value, group = id),
                              colour = "lightgrey",
                              linewidth = 1)
       } +
 
       {if ( !is.null(slcted_id) &  is.null(facet_by) )
-         geom_line(data = dplyr::filter(Labels, highlight == paste(slcted_id)),
+         geom_line(data = dplyr::filter(data, highlight == paste(slcted_id)),
                            aes(x = date, y = value, group = id),
                             size = 2.5)
       } +
 
       {if (!is.null(slcted_id) &  !is.null(facet_by) )
-         geom_line(data = dplyr::filter(Labels, highlight == paste(slcted_id)),
+         geom_line(data = dplyr::filter(data, highlight == paste(slcted_id)),
                             aes(x = date, y = value, group = id, colour = band),
                             size = 2.5)
       } +
 
 
       {if (!is.null(slcted_id) & show_Labels)
-         geom_point (data = dplyr::filter(Labels, highlight == paste(slcted_id) ),
+         geom_point (data = dplyr::filter(data, highlight == paste(slcted_id) ),
                               aes(x = date, y = y_lvl, colour = phenophase),
                               na.rm=TRUE, fontface = "bold", size = 2)
       } +
