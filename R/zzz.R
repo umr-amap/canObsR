7ceeb016711c9add420ab776949a60aee1d767a9
htulet <hadrien.tulet@cirad.fr>
2025-06-26 10:02:02 +0200

    test_init

--- R/zzz.R
+++ R/zzz.R
@@ -9,17 +9,8 @@
 #    }
 # }
 
-# .onLoad <- function(libname, pkgname) {
-#    library(reticulate)
-#
-#    env_name <- getOption("managecrownsdata.conda_env", "pipeline_test_R")
-#
-#    if (!(env_name %in% conda_list()$name)) {
-#       message("Creating conda environment '", env_name, "'...")
-#       conda_create(env_name, environment = "../environment.yaml")
-#    }
-#
-#    use_condaenv(env_name, required = TRUE)
-#
-#    source_python(system.file("__init__.py", package = "managecrownsdata"))
-# }
+.onLoad <- function(libname, pkgname) {
+   library(reticulate)
+
+   source_python(system.file("PYTHON/__init__.py", package = "canObsR"))
+}
